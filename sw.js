7bed0630fd4ec0a7bac31571c0daaea570d87364
chenfeng <chenfeng@momenta.ai>
2018-03-07 16:09:07 +0800

    rebuild

--- sw.js
+++ sw.js
@@ -1,15 +1,8 @@
 ---
 layout: compress
 ---
-var CACHE_NAME = 'madhur-cache-v1';
-var urlsToCache = [
-    'https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js',
-    'https://cdn.bootcss.com/mdui/0.2.1/js/mdui.min.js',
-    'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.js',
-    'https://cdn.bootcss.com/jquery_lazyload/1.9.7/jquery.lazyload.min.js',
-    'https://cdn.bootcss.com/mdui/0.2.1/css/mdui.min.css',
-    'https://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css',
-
+var cacheName = 'madhur-cache-v1';
+var filesToCache = [
     {% for page in site.pages %}
         {% if page.url contains 'projects' or page.url contains '404'   %}
             
@@ -24,55 +17,31 @@ var urlsToCache = [
     {% endfor %}
 
     {% for file in site.static_files %}
-        '{{ file.path }}',
+              '{{ file.path }}',
     {% endfor %}
 ];
+
+
 self.addEventListener('install', function(event) {
-  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
-    return cache.addAll(urlsToCache);
-  }).catch(function(err) {
-    console.log('Cache add error: ', err);
-  }));
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
     );
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    }).catch(function() {
-      return caches.match('/offline.html');
-    })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    console.log('[*] Serving cached: ' + event.request.url);
+                    return response;
+                }
+
+                console.log('[*] Fetching: ' + event.request.url);
+                return fetch(event.request);
+            }
+        )
+    );
 });
